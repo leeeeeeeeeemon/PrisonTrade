c3837286f2121c2b382db9477b3c8334cd6cc4ac
leeeeeeeeeemon <emilbarish@gmail.com>
2022-11-02 07:12:04 +0300

    Добавьте файлы проекта.

---
+++ PrisonTrade/Pages/MainPagePrisoner.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrisonTrade.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для MainPagePrisoner.xaml
+    /// </summary>
+    public partial class MainPagePrisoner : Page
+    {
+        public MainPagePrisoner()
+        {
+            InitializeComponent();
+        }
+
+        private void showPrisonerBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new PrisonersPage());
+        }
+    }
+}
---
+++ PrisonTrade/Pages/PrisonersPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PrisonTrade.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для PrisonersPage.xaml
+    /// </summary>
+    public partial class PrisonersPage : Page
+    {
+        public PrisonersPage()
+        {
+            InitializeComponent();
+        }
+
+        private void goBackBtn_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.GoBack();
+        }
+    }
+}
